00e2a0d8b6fea782bff87d9da12b02cb0dc2f439
GCBGST <123060152+Vnkyz@users.noreply.github.com>
2023-07-18 14:07:00 +0700

    Update product.php

--- product.php
+++ product.php
@@ -6,12 +6,12 @@
 session_start();
 
 // initializing variables
-$username = "";
-$email    = "";
+$ucp = "";
+$discordid = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'project');
+$db = mysqli_connect('localhost', 'root', 'lexyy@2609', 'uirp');
 
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
@@ -19,15 +19,15 @@ if ($db->connect_error) {
 
 
 
-$sql = "SELECT id, username, email, img FROM users";
+$sql = "SELECT ID, ucp, DiscordID FROM playerucp";
 $result = $db->query($sql);
 
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        print "<br> id: ". $row["id"]. "<br> - Name: ". $row["username"]. "<br> - Email: " . $row["email"] . "<br>";
-      print "<img src=\"".$row["img"]."\">";
+        print "<br> id: ". $row["ID"]. "<br> - Name: ". $row["ucp"]. "<br> - Discord ID: " . $row["DiscordID"] . "<br>";
+      //print "<img src=\"".$row["img"]."\">";
      
     }
 } else {
